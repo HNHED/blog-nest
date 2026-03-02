06a1ec52c0999577cf53c1d561ae159433114882
HNHED <1065420477@qq.com>
2026-03-02 23:05:24 +0800

    feat: deal with multiple domain from frontend

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', process.env.FRONTEND_DOMAIN],
+    origin: ['http://localhost:3000', ...(process.env.FRONTEND_DOMAIN ? process.env.FRONTEND_DOMAIN.split(',') : [])],
   })
   app.enableShutdownHooks()
   await app.listen(3333);
